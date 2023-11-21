6f71bdab060a5ec367537b361c7aba68b4fccd12
Riwo <c.heerink@riwo.eu>
2023-11-21 13:47:22 +0100

    update support gripper

--- arctos_moveit_config/launch/demo.launch.py
+++ arctos_moveit_config/launch/demo.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("arctos_description", package_name="arctos_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("arctos_urdf", package_name="arctos_moveit_config").to_moveit_configs()
     return generate_demo_launch(moveit_config)
--- arctos_moveit_config/launch/static_virtual_joint_tfs.launch.py
+++ arctos_moveit_config/launch/static_virtual_joint_tfs.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_laun
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("arctos_description", package_name="arctos_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("arctos_urdf", package_name="arctos_moveit_config").to_moveit_configs()
     return generate_static_virtual_joint_tfs_launch(moveit_config)
